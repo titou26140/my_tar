efffa4dd809a9b155710e5245192c3fc5a19bae3
Eléonore Goumy <eleonore.goumy@epitech.eu>
2017-03-03 21:09:38 +0100

    main

---
+++ main_arch.c
@@ -0,0 +1,20 @@
+/*
+** main_arch.c for main_arch in /home/e.goumy/my_tar
+** 
+** Made by Eléonore Goumy
+** Login   <e.goumy@epitech.net>
+** 
+** Started on  Fri Mar  3 20:47:48 2017 Eléonore Goumy
+** Last update Fri Mar  3 21:08:03 2017 Eléonore Goumy
+*/
+
+#include "include/tar.h"
+
+int	main(int ac, char **av)
+{
+  FILE	*f;
+
+  f = fopen("my_archive", "w+");
+  
+  return (0);
+}
